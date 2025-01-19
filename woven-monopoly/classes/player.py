325654918102135b78e9392c25f6e365c70a966d
oliver <oliver.markey@outlook.com>
2025-01-19 13:32:43 +1100

    Created Player class with attributes

---
+++ woven-monopoly/classes/player.py
@@ -0,0 +1,9 @@
+
+class Player:
+    def __init__(self, id: int, name: str, money: int, position: int = 0):
+        self.id = id
+        self.name = name
+        self.money = money
+        self.position = position
+
+
